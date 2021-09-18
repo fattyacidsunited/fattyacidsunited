682bc2d64ce4e711faa09699b338ed3ecfc4f2e8
fattyacidsunited <90957884+fattyacidsunited@users.noreply.github.com>
2021-09-18 13:56:11 +0530

    Create installme.py

---
+++ installme.py
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @fattyacidsunited
+- 👀 I’m interested in ... Cyber Security Checks, AI & ML 
+- 🌱 I’m currently learning ...Python and Computer Networks
+- 💞️ I’m looking to collaborate on ...India's own non-"Google Maps" 
+- 📫 How to reach me ...email- kushalrishi@gmail.com, ig- fattyacidsunited 
+
+# this file installme.py will appear on my home page! 
+# since I'm learning python, you can count me in syltherin? 
+print(type(kushal))
+o/p : you'll get to know 
